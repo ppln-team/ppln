a1f508556b879892b9a4833c47436f3ef65347be
Amir, Miras <miras.amir@x5.ru>
2019-09-10 15:36:09 +0300

    add before_script

--- imet/scripts/convert.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-DATA_PATH=/ppln/data/imet/
-
-PYTHONPATH=/ppln python /ppln/imet/convert.py \
-    --labels_path=${DATA_PATH}/labels.csv \
-    --annotation_path=${DATA_PATH}/train.csv \
-    --output_labels_path=${DATA_PATH}/sub_labels.csv \
-    --output_annotation_path=${DATA_PATH}/sub_train.csv
--- imet/scripts/dist_train.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-PYTHONPATH=/ppln CUDA_VISIBLE_DEVICES=0,1,2,3 \
-python -m torch.distributed.launch --nproc_per_node=$2 /ppln/imet/train.py $1 ${@:3}
