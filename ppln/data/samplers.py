160f84b9fea9560b0a981f236a20a22e894c51ac
miras.amir <miras.amir@x5.ru>
2019-09-18 15:38:55 +0300

    add WeightedRandomDistributedSampler

---
+++ ppln/data/samplers.py
@@ -0,0 +1,44 @@
+import math
+
+import torch
+import torch.distributed as dist
+from torch.utils.data import Sampler
+
+
+class WeightedRandomDistributedSampler(Sampler):
+    def __init__(self, dataset, weights, num_samples, replacement=True, num_replicas=None, rank=None):
+        if num_replicas is None:
+            if not dist.is_available():
+                raise RuntimeError('Requires distributed package to be available')
+            num_replicas = dist.get_world_size()
+        if rank is None:
+            if not dist.is_available():
+                raise RuntimeError('Requires distributed package to be available')
+            rank = dist.get_rank()
+        self.dataset = dataset
+        self.weights = weights
+        self.replacement = replacement
+        self.num_samples = int(math.ceil(num_samples) * 1.0 / self.num_replicas)
+        self.num_replicas = num_replicas
+        self.rank = rank
+        self.epoch = 0
+        self.total_size = self.num_samples * self.num_replicas
+
+    def __iter__(self):
+        # deterministically shuffle based on epoch
+        g = torch.Generator()
+        g.manual_seed(self.epoch)
+        indices = torch.multinomial(self.weights, self.total_size, self.replacement, generator=g).tolist()
+        assert len(indices) == self.total_size
+
+        # subsample
+        indices = indices[self.rank:self.total_size:self.num_replicas]
+        assert len(indices) == self.num_samples
+
+        return iter(indices)
+
+    def __len__(self):
+        return self.num_samples
+
+    def set_epoch(self, epoch):
+        self.epoch = epoch
