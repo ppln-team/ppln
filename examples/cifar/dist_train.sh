d5a15a22ac4eb3bb1b9802a79aabf9358eae5136
miras.amir <miras.amir@x5.ru>
2019-10-14 14:43:23 +0300

    fix bugs

--- examples/cifar/dist_train.sh
+++ examples/cifar/dist_train.sh
@@ -5,4 +5,4 @@ NUM_GPUS=$2
 
 CUDA_VISIBLE_DEVICES=0,1,2,3 \
 PYTHONPATH=/ppln:/ppln/examples \
-python -m torch.distributed.launch --nproc_per_node="$NUM_GPUS" train.py "$CONFIG_PATH" --launcher pytorch
+python -m torch.distributed.launch --nproc_per_node="$NUM_GPUS" train.py "$CONFIG_PATH"
