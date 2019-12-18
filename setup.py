785206ae5f0e9ad678f80d99d76e07f070654aaf
miras.amir <miras.amir@x5.ru>
2019-12-18 20:25:34 +0300

    fix install_requires

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
     packages=find_packages(),
     install_requires=[
         'torchvision', 'torch>=1.1', 'tqdm', 'PyYAML', 'colorama', 'addict', 'jpeg4py', 'opencv-python',
-        'albumentations'
+        'albumentations', 'tensorboard'
     ],
     setup_requires=['pytest-runner'],
     python_requires='>=3.6.0'
